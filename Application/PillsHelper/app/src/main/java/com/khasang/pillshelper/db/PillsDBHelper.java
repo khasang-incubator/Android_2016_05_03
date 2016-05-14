ad76382ae4988209b593cd9199e5ea96adde04ce
grolegor <grolegor@gmail.com>
2016-05-14 17:09:19 +0300

    turn PillsDBHelper in Singletone
    for more info see http://www.androiddesignpatterns.com/2012/05/correctly-managing-your-sqlite-database.html

--- Application/PillsHelper/app/src/main/java/com/khasang/pillshelper/db/PillsDBHelper.java
+++ Application/PillsHelper/app/src/main/java/com/khasang/pillshelper/db/PillsDBHelper.java
@@ -9,7 +9,20 @@ public class PillsDBHelper extends SQLiteAssetHelper {
     private static final String DATABASE_NAME = "pills.db";
     private static final int DATABASE_VERSION = 1;
 
-    public PillsDBHelper(Context context) {
+    private static volatile PillsDBHelper instance = null;
+
+    public static PillsDBHelper getInstance(Context context){
+        if(instance == null){
+            synchronized (PillsDBHelper.class){
+                if(instance == null){
+                    return new PillsDBHelper(context.getApplicationContext());
+                }
+            }
+        }
+        return instance;
+    }
+
+    private PillsDBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 }
