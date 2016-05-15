e40ee4fd8db4567d762d149f224515a0b718fa92
DarkLab <allihov67@gmail.com>
2016-05-15 16:17:47 +0300

    add generic request; add scrolling activity

---
+++ Application/PillsHelper/app/src/main/java/com/khasang/pillshelper/Drug.java
@@ -0,0 +1,35 @@
+package com.khasang.pillshelper;
+
+/**
+ * Created by aleksandrlihovidov on 14.05.16.
+ */
+public class Drug {
+    private int id;
+    private String name;
+
+    public Drug(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
--- Application/PillsHelper/app/src/main/java/com/khasang/pillshelper/db/PillsDBHelper.java
+++ Application/PillsHelper/app/src/main/java/com/khasang/pillshelper/db/PillsDBHelper.java
@@ -1,10 +1,30 @@
 package com.khasang.pillshelper.db;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.TextView;
 
+import com.khasang.pillshelper.Drug;
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+
 public class PillsDBHelper extends SQLiteAssetHelper {
+    public static final int RESULT_OK = 1;
+    public static final int RESULT_ERROR = 2;
+
+    public static final int INITIALIZE_DB = 1;
+    public static final int GET_ALL_DRUGS = 2;
 
     private static final String DATABASE_NAME = "pills.db";
     private static final int DATABASE_VERSION = 1;
@@ -15,7 +35,7 @@ public class PillsDBHelper extends SQLiteAssetHelper {
         if(instance == null){
             synchronized (PillsDBHelper.class){
                 if(instance == null){
-                    return new PillsDBHelper(context.getApplicationContext());
+                    instance = new PillsDBHelper(context.getApplicationContext());
                 }
             }
         }
@@ -25,4 +45,74 @@ public class PillsDBHelper extends SQLiteAssetHelper {
     private PillsDBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
+
+    private Boolean initializeDB() {
+        SQLiteDatabase db = getReadableDatabase();
+        if (db != null) {
+            db.close();
+            return Boolean.TRUE;
+        }
+        return Boolean.FALSE;
+    }
+
+    private ArrayList<Drug> getAllDrugs() {
+        ArrayList<Drug> drugs = new ArrayList<>();
+        SQLiteDatabase db = getReadableDatabase();
+
+        Cursor c = db.query("drug", new String[]{"_id", "name"}, null, null, null, null, null);
+        c.moveToFirst();
+        while (!c.isAfterLast()) {
+            int id = c.getInt(0);
+            String name = c.getString(1);
+
+            drugs.add(new Drug(id, name));
+
+            c.moveToNext();
+        }
+
+        c.close();
+        db.close();
+
+        return drugs;
+    }
+
+    public <T> void runRequestAsync(int request, final Listener<T> listener) {
+
+        new DataFetcher<T>(listener).execute(request);
+    }
+
+    public interface Listener<T> {
+        void onHelperAnswerReceived(int code, T response);
+    }
+
+    public class DataFetcher<T> extends AsyncTask<Integer, Void, T> {
+        Listener<T> listener;
+
+        public DataFetcher(Listener<T> listener) {
+            this.listener = listener;
+        }
+
+        @Override
+        protected T doInBackground(Integer... params) {
+            int request = params[0];
+
+            switch (request) {
+                case GET_ALL_DRUGS: return (T) getAllDrugs();
+                case INITIALIZE_DB: return (T) initializeDB();
+
+                default: return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(T t) {
+            if (t != null) {
+                listener.onHelperAnswerReceived(RESULT_OK, t);
+            } else {
+                listener.onHelperAnswerReceived(RESULT_ERROR, null);
+            }
+
+        }
+    }
+
 }
