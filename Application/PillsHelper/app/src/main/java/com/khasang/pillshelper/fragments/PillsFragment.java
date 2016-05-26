c06eaf4f0f4ad6cf0db424a7fcd69b4dc0dbcd48
grolegor <grolegor@gmail.com>
2016-05-26 13:32:21 +0300

    create drug list

--- Application/PillsHelper/app/src/main/java/com/khasang/pillshelper/fragments/PillsFragment.java
+++ Application/PillsHelper/app/src/main/java/com/khasang/pillshelper/fragments/PillsFragment.java
@@ -3,24 +3,78 @@ package com.khasang.pillshelper.fragments;
 
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.khasang.pillshelper.R;
+import com.khasang.pillshelper.db.PillsDBHelper;
+import com.khasang.pillshelper.db.model.Drug;
+
+import java.util.List;
 
 public class PillsFragment extends Fragment {
 
 
     public PillsFragment() {
-        // Required empty public constructor
     }
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_pills, container, false);
+        View view = inflater.inflate(R.layout.fragment_pills, container, false);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.drug_list);
+
+        mLayoutManager = new LinearLayoutManager(view.getContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        PillsDBHelper.init(view.getContext());
+        //mAdapter = new MyAdapter(PillsDBHelper.getInstance().getAllDrugs());
+        mAdapter = new MyAdapter(PillsDBHelper.getInstance().findDrugsByName("клостилбегит"));
+        mRecyclerView.setAdapter(mAdapter);
+        return view;
+    }
+}
+
+class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private List<Drug> mDataset;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView mTextView;
+        public ViewHolder(TextView v) {
+            super(v);
+            mTextView = v;
+        }
     }
 
+    public MyAdapter(List<Drug> myDataset) {
+        mDataset = myDataset;
+    }
+
+    @Override
+    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        TextView v = (TextView) LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.mTextView.setText(mDataset.get(position).getName());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
 }
