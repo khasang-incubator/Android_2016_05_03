17f6ebc40ddc7cbc90bd49066de2ab4ca48403b4
Miki48RU <miki48ru@gmail.com>
2016-06-01 18:12:01 +0300

    Revert "вывод логики в Data"
    This reverts commit ecc9e34d16dfb49ce998e5c7fbaf02b1bf376d36.

--- Application/PillsHelper/app/src/main/java/com/khasang/pillshelper/MainActivity.java
+++ Application/PillsHelper/app/src/main/java/com/khasang/pillshelper/MainActivity.java
@@ -2,32 +2,36 @@ package com.khasang.pillshelper;
 
 import android.app.Activity;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ShareActionProvider;
 
-import android.widget.CalendarView;
-
-import android.widget.Toast;
-
+import com.khasang.pillshelper.fragments.AllCourseFragment;
+import com.khasang.pillshelper.fragments.CurrentCourseFragment;
+import com.khasang.pillshelper.fragments.MainFragment;
+import com.khasang.pillshelper.fragments.NewCourseFragment;
 
 public class MainActivity extends Activity {
-    private CalendarView calendarView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        calendarView = (CalendarView) findViewById(R.id.calendarView_all_course_test);
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-                Toast.makeText(getApplicationContext(),
-                        "Год: " + year + "\n" +
-                                "Месяц: " + month + "\n" +
-                                "День: " + dayOfMonth,
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
+
     }
 
 
